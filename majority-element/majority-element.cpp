ce069b6ce65937a3307c80c688be728e83570917
Sahil Kumar <134430230+SahilKumar601@users.noreply.github.com>
2024-11-22 15:44:15 +0530

    feat: update cpp solution for majority-element

--- majority-element/majority-element.cpp
+++ majority-element/majority-element.cpp
@@ -4,12 +4,12 @@ public:
         int n = nums.size();
         int major = nums[0];      
         int count = 1;
-        for(int i=0;i<n;i++){
+        for(int i=1;i<n;i++){
             if(count == 0){
                 count++;
                 major = nums[i];
             }
-            if(major == nums[i]){
+            else if(major == nums[i]){
                 count++;
             }else {
                 count--;

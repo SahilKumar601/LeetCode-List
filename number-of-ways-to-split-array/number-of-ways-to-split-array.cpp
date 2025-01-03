6cb354b063c455dfe36db1ceef6517333d56c904
Sahil Kumar <134430230+SahilKumar601@users.noreply.github.com>
2025-01-04 01:58:27 +0530

    feat: update cpp solution for number-of-ways-to-split-array

--- number-of-ways-to-split-array/number-of-ways-to-split-array.cpp
+++ number-of-ways-to-split-array/number-of-ways-to-split-array.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
-    static int waysToSplitArray(vector<int>& nums) {
-        const int n=nums.size();
-        long long sum=accumulate(nums.begin(), nums.end(), 0LL);
-        long long acc=0LL;
-        int cnt=0;
-        for(int i=0; i<n-1; i++){
-            acc+=nums[i];
-            cnt+=(2*acc>=sum);
+    int waysToSplitArray(vector<int>& nums) {
+        int n = nums.size();
+        long long sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
         }
-        return cnt;
+        long long inLoop=0;
+        int result=0;
+        for(int i=0;i<n-1;i++){
+            inLoop+=nums[i];
+            if(inLoop >= sum-inLoop){
+                result++;
+            }
+        }
+        return result;
     }
 };
